c328bf6481e11e6588c6aa7373ff70faf67ce46a
Sungwoo Park <tjddneva@naver.com>
2022-02-11 15:48:08 +0900

    client domain, repository and test

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,68 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach  //각각의 test case 끝날 때 마다 실행 -> test case 순서 상관 없이 돌아갈 수 있게
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring"); //command shift enter 치면 중간에서 밑줄로 내려감
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        //System.out.println("result = " + (result == member));
+        //Assertions.assertEquals(member,result);
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member(); //shift f6 하고 이름 바꾸면 한번에 다 바뀜
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+        assertThat(result).isEqualTo(member1);
+
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+
+    }
+
+}
+
+// Test 먼저 만들고 개발 하는 거 -> TDD test driven development ?
